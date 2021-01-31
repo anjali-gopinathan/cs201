2a88724ab554540f8ab0212cec6945549eb2aa48
Anjali Gopinathan <asgopina@usc.edu>
2021-01-31 02:18:05 -0800

    menu options 1,2,3,7 pretty much done, formatting not yet

--- asgopina_CS201L_Assignment1/src/asgopina_CS201L_Assignment1/Assignment1.java
+++ asgopina_CS201L_Assignment1/src/asgopina_CS201L_Assignment1/Assignment1.java
@@ -1,12 +1,17 @@
 package asgopina_CS201L_Assignment1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Map;
@@ -14,6 +19,8 @@ import java.util.Scanner;
 import java.util.zip.DataFormatException;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 
 public class Assignment1 {
 	private static Boolean isDateFormat(String dateStr) {
@@ -22,66 +29,12 @@ public class Assignment1 {
 		try {
 			Date d = sdf.parse(dateStr);
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
 			isDate = false;
-//			e.printStackTrace();
 		}
 		return isDate;
 	}
-	private static List<Stock> getStocksList(Scanner scan){
-		List<Stock> stocks = new ArrayList<Stock>();
-		Gson gson = new Gson();			
-		Boolean badInput;
-		FileReader fr;
-		BufferedReader br;
-		String inputFilename;
-		Map<String, List<Map<String, String>>> map;
-		List<Map<String, String>> stocks_listOfMaps = null;
-		String dateStr = "";
-		do {
-			badInput = false;
-			System.out.print("What is the name of the company file? ");
-			inputFilename = scan.next();		
-			try {
-				fr = new FileReader(inputFilename);
-				br = new BufferedReader(fr);
-				// First read in json as map with one element (mapping from the single key, "data", to a map of Stock objects)
-				map = gson.fromJson(br, Map.class);
-				stocks_listOfMaps = map.get("data"); 
-				fr.close();
-				br.close();
-				
-				for(int i=0; i<stocks_listOfMaps.size(); i++) {
-					Map<String, String> thisStockMap = stocks_listOfMaps.get(i);
-					dateStr = thisStockMap.get("startDate");
-					if(!isDateFormat(dateStr)) {
-						throw new DataFormatException();
-					}
-					Stock s = new Stock(thisStockMap.get("name"),
-										thisStockMap.get("ticker"),
-										dateStr,
-										thisStockMap.get("description"),
-										thisStockMap.get("exchangeCode"));
-					stocks.add(s);
-				}
-				
-				
-			} catch (IOException e) {
-				System.out.println("The file " + inputFilename + " could not be found.\n");
-				badInput = true;
-			} catch (DataFormatException e) {
-				// TODO Auto-generated catch block
-				System.out.println("The startDate " + dateStr + " cannot be converted to the proper date format of YYYY-MM-DD.");
-				badInput = true;
-				//				e.printStackTrace();
-			}
-		} while(badInput);
-		System.out.println("The file has been properly read.\n");
-
-//			System.out.println("Stocks:\n" + stocks);
-
-		return stocks;
-	}
+//	private static List<Stock> getStocksList(Scanner scan, Gson gson){
+//	}
 	private static int getUserChoice(Scanner scan) {
 		int choice = 0;
 		do {
@@ -98,13 +51,13 @@ public class Assignment1 {
 			}
 			catch(InputMismatchException e) {
 				System.out.println("\nThat is not a valid option.\n");
-				scan.next();	//discard non-int input
+				scan.next();	//discard non integer input
 				continue;
 			}
 		} while (true);
 		return choice;
 	}
-	private static int findStockByTicker(List<Stock> stocks, String ticker) {
+	private static int choice2_findStockByTicker(List<Stock> stocks, String ticker) {
 		int idx = -1;
 		// linear search to find matching ticker symbol
 		for(int i=0; i<stocks.size(); i++) {
@@ -115,115 +68,190 @@ public class Assignment1 {
 		}
 		return idx;
 	}
-	public static void main(String[] args) {
-//		Scanner scan = new Scanner(System.in);
-		Scanner scan = new Scanner(System.in);
-
-//		System.out.print("What is the name of the company file? ");
-//		String inputFilename = scan.next();
-		List<Stock> stocks = getStocksList(scan);
-		
-		System.out.println(	"\t1) Display all public companies\n" +
-							"\t2) Search for a stock (by ticker)\n" +
-							"\t3) Search for all stocks on an exchange\n" + 
-							"\t4) Add a new company/stocks\n" + 
-							"\t5) Remove a company\n" + 
-							"\t6) Sort companies\n" + 
-							"\t7) Exit");
-		
-		int choice = getUserChoice(scan);
-		Boolean badInput;
-		String tickerTarget = null;
-		switch(choice) {
-			case 1:
-				// display all public companies
-				for(Stock s : stocks) {
-					System.out.println(s);
-				}
-				break;
-			case 2:
-				// search for a stock by ticker
-				int foundIndex;
-				do {
-					badInput = false;
-					System.out.println("What is the name of the company you would like to search for? ");
-					tickerTarget = scan.next().toLowerCase();
-					foundIndex = findStockByTicker(stocks, tickerTarget);
-					if (foundIndex == -1) {
-						System.out.println(tickerTarget + " could not be found.\n");
-						badInput = true;
-					}
-				} while(badInput);
-				Stock foundStock = stocks.get(foundIndex);
-				System.out.println(foundStock.getName() + ", started on " + foundStock.getStartDate() + ", listed on " + foundStock.getExchangeCode());
-				break;
-//			case 7:
-			default:
-				break;
-				
+	private static Boolean choice3_findStocksByExchange(List<Stock> stocks, String exchangeTarget) {
+		Boolean badInput = false;
+		List<String> result = new ArrayList<String>();
+		for (int i=0; i<stocks.size(); i++) {
+			Stock thisStock = stocks.get(i);
+			if (thisStock.getExchangeCode().toLowerCase().equals(exchangeTarget)) {
+				result.add(thisStock.getName());
+			}
 		}
-	}
-
-}
-class Stock{
-	private String name;
-	private String ticker;
-	private String startDate;
-	private String description;
-	private String exchangeCode;
-	
-	public Stock() {
+		badInput = result.size() == 0;		// if no stocks get added to the list,
 		
+		if(badInput) {
+			System.out.println("No exchange named " + exchangeTarget + " found.");
+		}
+		else {
+			String result_commaSeparated = String.join(",", result);
+			System.out.println(result_commaSeparated + " found on the " + exchangeTarget + " exchange.");
+		}
+		
+		return badInput;
 	}
-	public Stock(String name, String ticker, String startDate, String description, String exchangeCode) {
-		setName(name);
-		setTicker(ticker);
-		setStartDate(startDate);
-		setDescription(description);
-		setExchangeCode(exchangeCode);
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public void setTicker(String ticker) {
-		this.ticker = ticker;
-	}
-	public void setStartDate(String startDate) {
-		this.startDate = startDate;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public void setExchangeCode(String exchangeCode) {
-		this.exchangeCode = exchangeCode;
-	}
-	public String getName() {
-		return this.name;
-	}
-	public String getTicker() {
-		return this.ticker;
-	}
-	public String getStartDate() {
-		return this.startDate;
-	}
-	public String getDescription() {
-		return this.description;
-	}
-	public String getExchangeCode() {
-		return this.exchangeCode;
-	}
-	@Override
-	public String toString() {
-//		return "Stock [name=" + name + ", ticker=" + ticker + ", startDate=" + startDate + ", description="
-//				+ description + ", exchangeCode=" + exchangeCode + "]";
-//		String desc_multilineString = WordUtils.wrap(this.description, 80);
+	private static void choice7_exit(Scanner scan, List<Stock> stocks, String inputFilename) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-		return	this.name + 
-				", symbol " + this.ticker + 
-				", started on " + this.startDate +
-				", listed on " + this.exchangeCode + 
-		      	",\n\t" + this.description;
-
+		Boolean badInput = false;
+		FileWriter wr;
+		BufferedWriter bw;
+		int choice;
+		do {
+			System.out.println("\t1) Yes\n\t2) No");
+			System.out.println("\nWould you like to save your edits? ");
+			if (scan.hasNextInt()) {
+				choice = scan.nextInt();
+				if (choice == 1) {
+					// save stocks to JSON
+					try {
+						wr = new FileWriter(inputFilename);
+						bw = new BufferedWriter(wr);
+						Map<String, List<Stock>> map = new HashMap<String, List<Stock>>();
+						map.put("data", stocks);
+						gson.toJson(map, bw);
+						bw.flush();
+						bw.close();
+						System.out.println("\nYour edits have been saved to " + inputFilename);
+					} catch (IOException e) {
+						System.out.println("Error writing to file");
+//						e.printStackTrace();
+					}	
+				}
+				else if (choice != 2){
+					badInput = true;
+				}
+			}
+			else {
+				badInput = true;
+			}
+		} while(badInput);
+		System.out.println("\nThank you for using my program!");
 	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Gson gson = new Gson();			
+
+		List<Stock> stocks = new ArrayList<Stock>();
+		Boolean badInput;
+		FileReader fr;
+		BufferedReader br;
+		String inputFilename;
+		Map<String, List<Map<String, String>>> map;
+		List<Map<String, String>> stocks_listOfMaps = null;
+		String dateStr = "";
+		Boolean done = false;
+
+		do {
+			badInput = false;
+			System.out.print("What is the name of the company file? ");
+			inputFilename = scan.next();		
+			try {
+				fr = new FileReader(inputFilename);
+				br = new BufferedReader(fr);
+				// First read in JSON as map with one element (mapping from the single key, "data", to a map of Stock objects)
+				try {
+					map = gson.fromJson(br, Map.class);
+				} catch (Exception e) {
+					e.printStackTrace();
+					throw new DataFormatException();
+				}
+				stocks_listOfMaps = map.get("data"); 
+				fr.close();
+				br.close();
+				
+				for(int i=0; i<stocks_listOfMaps.size(); i++) {
+					Map<String, String> thisStockMap = stocks_listOfMaps.get(i);
+					dateStr = thisStockMap.get("startDate");
+					if(!isDateFormat(dateStr)) {
+						System.out.println("The startDate " + dateStr + " cannot be converted to the proper date format of YYYY-MM-DD.");
+						throw new DataFormatException();
+					}
+					Stock s = new Stock(thisStockMap.get("name"),
+										thisStockMap.get("ticker"),
+										dateStr,
+										thisStockMap.get("description"),
+										thisStockMap.get("exchangeCode"));
+					stocks.add(s);
+				}
+				
+				
+			} catch (IOException e) {
+				System.out.println("The file " + inputFilename + " could not be found.\n");
+				badInput = true;
+			} catch (DataFormatException e) {
+				System.out.println("The file " + inputFilename + " has bad format.\n");
+				badInput = true;
+			}
+		} while(badInput);
+		System.out.println("The file has been properly read.\n");
+
+	
+		
+		do {
+			System.out.println(	"\t1) Display all public companies\n" +
+								"\t2) Search for a stock (by ticker)\n" +
+								"\t3) Search for all stocks on an exchange\n" + 
+								"\t4) Add a new company/stocks\n" + 
+								"\t5) Remove a company\n" + 
+								"\t6) Sort companies\n" + 
+								"\t7) Exit");
+			
+			int choice = getUserChoice(scan);
+			switch(choice) {
+				case 1:
+					// display all public companies
+					for(Stock s : stocks) {
+						System.out.println(s);
+					}
+					break;
+				case 2:
+					String tickerTarget = null;
+					// search for a stock by ticker
+					int foundIndex;
+					if (stocks.size() > 0) {
+						do {
+							badInput = false;
+							System.out.println("What is the name of the company you would like to search for? ");
+							tickerTarget = scan.next().toLowerCase();
+							foundIndex = choice2_findStockByTicker(stocks, tickerTarget);
+							if (foundIndex == -1) {
+								System.out.println(tickerTarget + " could not be found.\n");
+								badInput = true;
+							}
+						} while(badInput);
+						Stock foundStock = stocks.get(foundIndex);
+						System.out.println(foundStock.getName() + ", started on " + foundStock.getStartDate() + ", listed on " + foundStock.getExchangeCode());
+					}
+					else {
+						System.out.println("Sorry, there are no stocks.");
+					}
+					break;
+				case 3:
+					String exchangeTarget;
+//					Boolean foundExchange;
+					// search for a stock by ticker
+					if (stocks.size() > 0) {
+						do {
+							badInput = false;
+							System.out.println("What Stock Exchange would like to search for? ");
+							exchangeTarget = scan.next().toLowerCase();
+							badInput = choice3_findStocksByExchange(stocks, exchangeTarget);
+						} while(badInput);
+					}
+					else {
+						System.out.println("Sorry, there are no stocks.");
+					}
+					
+					break;
+				case 7:
+					done = true;
+					choice7_exit(scan, stocks, inputFilename);
+					break;
+				default:
+					break;
+			}
+		} while(!done);
+	}
+
 
 }
