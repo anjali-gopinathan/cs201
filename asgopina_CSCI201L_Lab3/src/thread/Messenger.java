492e6fb9860671aadef7181797155238bdc6e74c
Anjali Gopinathan <asgopina@usc.edu>
2021-02-09 17:11:34 -0800

    done thread lab.

--- asgopina_CSCI201L_Lab3/src/thread/Messenger.java
+++ asgopina_CSCI201L_Lab3/src/thread/Messenger.java
@@ -9,8 +9,8 @@ public class Messenger extends Thread {
 	public void run() {
 		for(int i=0; i<20; i++) {
 			String msg = "message #" + i;
-			messageQueue.addMessage(msg);
 			System.out.println(Util.getCurrentTime() + " Messenger - insert \"" + msg + "\"");
+			messageQueue.addMessage(msg);
 			
 			try {
 				Thread.sleep((long)(Math.random() * 1000));		// sleep for random amount of time btwn 0 and 1 seconds
